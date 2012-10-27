cf2e5f25564e77a3d184a394ef1bf95d497658a2
Alan Larkin <alan.larkin@gmail.com>
2012-10-27 17:40:49 +0100

    Added a note.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,10 @@ class Ability
   def initialize(user)
     user ||= User.new
 
+    # NOTE: See the "Block Conditions with Scopes" section of
+    #       https://github.com/ryanb/cancan/wiki/Defining-Abilities-with-Blocks
+    #       for an explanation of the DSL syntax used below.
+
     # Comment out either one of the following 2 rules to avoid the error.
 
     # Comment me out and you should see a 2 document response.
